36161ae38484d67450b44e6f48c2a11194e0c8ab
Hershell Lucas <hershellsousa@gmail.com>
2024-12-25 17:57:11 -0300

    AulaUm

--- anatomia-classes-java/src/lucas/aprendizado/AulaUm.java
+++ anatomia-classes-java/src/lucas/aprendizado/AulaUm.java
@@ -4,7 +4,7 @@ public class AulaUm {
 
     public static void main (String[] args) {
     
-        String primeiroNome = "Hershell";
+        String primeiroNome = "H";
         String segundoNome = "Lucas";
     
         String meu_Nome = meuNome (primeiroNome,segundoNome);
