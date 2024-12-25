1279b4969f961b2b2b8bec15523d571925bdba2e
Hershell Lucas <hershellsousa@gmail.com>
2024-12-25 01:47:56 -0300

    meu primeiro commit em java

---
+++ anatomia-classes-java/src/AulaUm.java
@@ -0,0 +1,17 @@
+public class AulaUm {
+
+
+public static void main (String[] args) {
+
+    String primeiroNome = "Hershell";
+    String segundoNome = "Lucas";
+
+    String meu_Nome = meuNome (primeiroNome,segundoNome);
+    System.out.println(meu_Nome.toUpperCase() + " prazer em conhecer vocês!");
+}
+
+public static String meuNome (String primeiroNome, String segundoNome) {
+    return "O resultado desse teste agora é. " + primeiroNome.concat(" ").concat(segundoNome);
+}
+
+}
